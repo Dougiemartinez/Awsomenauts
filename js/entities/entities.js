cb3a72a2038636dfd3a3a97300f5dd029287d8b2
Douglas Martinez <dougie.martinez.m@gmail.com>
2015-01-23 12:29:47 -0800

    watched video "Awesomenauts 7 Moving your Player" added code to to the entity.js file to make my player move

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,7 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, {
+                //this code sets the hight and width of the players
                 image: "player",
                 width: 64,
                 height: 64,
@@ -10,9 +11,22 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
         }]);
+    
+        this.body.setVelocity(5, 0);
+    
     },
     
-    update: function(){
+    update: function(delta){
+        if(me.input.isKeyPressed("right")){
+            //adds to the position of my ex by adding the velocity defined above in Setvelocity() and multiplying it by
+            //me.timer.tick. makes the movement look smooth
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }else{
+            this.body.vel.x = 0;
+        }
         
+        this.body.update(delta);
+        return true;
     }   
+    
 });
