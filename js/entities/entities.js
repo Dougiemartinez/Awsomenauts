3b07ec13299fead8f7f063fabcf7280d88fdaf95
Douglas Martinez <dougie.martinez.m@gmail.com>
2015-01-26 13:22:19 -0800

    added comments for video 8

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,7 +11,7 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
         }]);
-    //this code changes the x and y values of our player
+    //this code changes the x and y values of our player 
         this.body.setVelocity(5, 20);
     
     },
