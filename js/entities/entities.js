7d9112fd868b3473494b576dc6777a5a2dde8baa
Douglas Martinez <dougie.martinez.m@gmail.com>
2015-01-27 12:15:43 -0800

    watched video "Awesmenauts 10 Animating your Payer" added code to entities.js to move our player and add animation.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,6 +13,11 @@ game.PlayerEntity = me.Entity.extend({
         }]);
     //this code changes the x and y values of our player 
         this.body.setVelocity(5, 20);
+        
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        
+        this.renderable.setCurrentAnimation("idle");
     
     },
     
@@ -21,11 +26,21 @@ game.PlayerEntity = me.Entity.extend({
             //adds to the position of my ex by adding the velocity defined above in Setvelocity() and multiplying it by
             //me.timer.tick. makes the movement look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
         }else{
             this.body.vel.x = 0;
         }
+        if(this.body.vel.x !== 0){
+        if(!this.renderable.isCurrentAnimation("walk")){
+           this.renderable.setCurrentAnimation("walk");
+        }
+    }else{
+        this.renderable.setCurrentAnimation("idle")
         
+    }
         this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
         return true;
     }   
     
